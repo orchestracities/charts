cfd3cbe48e61c1377d92fd5c6cc6807a59886029
Federico M. Facca <federico.facca@gmail.com>
2019-06-05 16:15:48 +0200

    add fetch to git-push

--- git-push.sh
+++ git-push.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+git fetch
 for branch in $(git branch -r|grep -v HEAD) ; do
     git checkout ${branch#origin/}
 done
