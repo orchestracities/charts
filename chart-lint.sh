3ac271cf5d36bf7b65de03da6d3282b727a77ce2
Federico M. Facca <chicco785@users.noreply.github.com>
2019-06-05 19:03:18 +0200

    data collector 0.1.0 (#23)
    * data collector 0.1.0
    
    * renamed correctly values file
    
    * fix chart-lint.sh
    
    * update README for the repo
    
    * fix .travis.yaml

--- chart-lint.sh
+++ chart-lint.sh
@@ -4,5 +4,17 @@ FILES=./charts/*
 for f in $FILES
 do
   echo "Processing $f chart..."
-  docker run -ti --rm -v $(pwd):/apps alpine/helm:2.9.0 lint $f
+  output=$(docker run -ti --rm -v $(pwd):/apps alpine/helm:2.9.0 lint $f 2>&1)
+  ret=$?
+
+  if [[ $ret -eq 0 ]]; then
+      echo $output
+  else
+      if [[ $output == *'error'* ]]; then
+           echo $output
+           exit 1
+      else
+           echo $output
+      fi
+  fi
 done
