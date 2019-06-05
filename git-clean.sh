98a48f31c43b3d3f86355f3ccb3405ec0859be43
Federico M. Facca <federico.facca@gmail.com>
2019-06-05 16:58:21 +0200

    fix clean script

--- git-clean.sh
+++ git-clean.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
 git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
-git fetch
-for branch in $(git branch -r|grep -v HEAD) ; do
-    git checkout ${branch#origin/}
-done
-git checkout $TRAVIS_BRANCH
