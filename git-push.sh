2d79e2040a2af32852560bd41244d85ced26dfa6
Federico M. Facca <federico.facca@gmail.com>
2019-06-05 17:14:26 +0200

    test deploy updates

--- git-push.sh
+++ git-push.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 echo "git push"
 echo "PR - ${TRAVIS_PULL_REQUEST}"
+rm -rf charts
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 git checkout gh-pages
 git checkout -b "PR-${TRAVIS_PULL_REQUEST}" gh-pages
 docker run -ti --rm -v $(pwd):/apps -v $(pwd)/local:/root/.helm/repository/local alpine/helm:2.9.0 repo index . --url https://orchestracities.github.io/charts/
 git add --all && git commit -m "PR - ${TRAVIS_PULL_REQUEST} update"
-git push origin "PR-${TRAVIS_PULL_REQUEST}"
+git "https://${GITHUB_TOKEN}@${GH_REPO}" "PR-${TRAVIS_PULL_REQUEST}" > /dev/null 2>&1
 #git merge origin/gh-pages
