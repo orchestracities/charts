a809067a5b074ec48a5256c216d1b99e23751c3d
Federico M. Facca <chicco785@users.noreply.github.com>
2020-07-31 12:16:25 +0200

    add support for affinity (#77)

--- charts/quantumleap/deploy-redis.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-helm install --name redis --set usePassword=false --namespace prod stable/redis
-
-# Need to debug?
-# kubectl run --namespace dev redis-redis-client --rm --tty -i --image bitnami/redis:4.0.6-r1 -- bash
-# redis-cli -h redis-redis
